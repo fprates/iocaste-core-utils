d01177eb7f122cd225f7836cd2e972f650e853c5
Francisco Prates <mad.kanie@gmail.com>
2011-10-27 01:32:21 -0200

    script: o nome da tabela agora tambem e declarado.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- scripts/db-install.sql
+++ scripts/db-install.sql
@@ -7,7 +7,7 @@ delete from docs003 where ename='user_name';
 delete from docs003 where ename='user_secret';
 delete from docs002 where docid='user';
 
-insert into docs001 (docid) values ('user');
+insert into docs001 (docid, tname) values ('user', 'users001');
 insert into docs003 (ename, decim, lngth, etype) values ('user_name', 0, 12, 0);
 insert into docs003 (ename, decim, lngth, etype) values ('user_secret', 0, 12, 0);
 insert into docs002 (iname, docid, ename, attrb) values ('name', 'user', 'user_name', 'username');
