e874ea34ba49a25ea3439cd5bd5055eedaff376b
Francisco Prates <mad.kanie@gmail.com>
2011-10-28 12:45:49 -0200

    scripts: extende informaçoes do modelo "user".
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- scripts/db-install.sql
+++ scripts/db-install.sql
@@ -7,11 +7,15 @@ delete from docs003 where ename='user_name';
 delete from docs003 where ename='user_secret';
 delete from docs002 where docid='user';
 
-insert into docs001 (docid, tname) values ('user', 'users001');
+insert into docs001 (docid, tname, class) values ('user', 'users001', 'org.iocaste.protocol.user.User');
 insert into docs003 (ename, decim, lngth, etype) values ('user_name', 0, 12, 0);
 insert into docs003 (ename, decim, lngth, etype) values ('user_secret', 0, 12, 0);
-insert into docs002 (iname, docid, ename, attrb) values ('name', 'user', 'user_name', 'username');
-insert into docs002 (iname, docid, ename, attrb) values ('secret', 'user', 'user_secret', 'secret');
+insert into docs003 (ename, decim, lngth, etype) values ('user_firstname', 0, 64, 0);
+insert into docs003 (ename, decim, lngth, etype) values ('user_surname', 0, 64, 0);
+insert into docs002 (iname, docid, fname, ename, attrb) values ('name', 'user', 'UNAME', 'user_name', 'username');
+insert into docs002 (iname, docid, fname, ename, attrb) values ('secret', 'user', 'SECRT', 'user_secret', 'secret');
+insert into docs002 (iname, docid, fname, ename, attrb) values ('firstname', 'user', 'FNAME', 'user_firstname', 'firstname');
+insert into docs002 (iname, docid, fname, ename, attrb) values ('surname', 'user', 'SNAME', 'user_surname', 'surname');
 insert into docs004 (docid, iname) values ('user', 'name');
 
 commit work;
