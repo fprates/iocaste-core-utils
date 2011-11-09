ccaf88a063fe4a2688b773bfb160ba018bb44315
Francisco Prates <mad.kanie@gmail.com>
2011-11-09 13:11:15 -0200

    scripts: correção de script windows para instalação.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- install.bat
+++ install.bat
@@ -1,6 +1,5 @@
 @echo off
-cd ..\hsqldb\bin
-set SQLTOOL="java -Dsqlfile.charset=UTF-8 -jar ..\lib\sqltool.jar"
-set DB_SCRPT_PATH=..\..\scripts
-set DB_SCRIPTS=%DB_SCRPT_PATH%\db-install-*.sql
-%SQLTOOL% localhost-sa %DB_SCRPT_PATH%
+set SQLTOOL=java -Dsqlfile.charset=UTF-8 -jar ..\hsqldb\lib\sqltool.jar
+%SQLTOOL% localhost-sa scripts\db-install.sql
+copy *.war ..\apache-tomcat\webapps\
+echo "Instalação concluída com sucesso."
